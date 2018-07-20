54cf37b2b10438467b30d59ef17fc23e69df7c0e
yugisoft <yugisoftware@gmail.com>
2018-07-20 11:36:41 +0300

    TEST

--- app/src/main/java/library/yugisoft/library/yugiTest.java
+++ app/src/main/java/library/yugisoft/library/yugiTest.java
@@ -2,8 +2,6 @@ package library.yugisoft.library;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import library.yugisoft.module.yugi;
 
@@ -15,24 +13,12 @@ public class yugiTest extends yugi.vActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Array of choices
-        String colors[] = {"Red","Blue","White","Yellow","Black", "Green","Purple","Orange","Grey"};
-
-// Selection of the spinner
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
 
-
-// Application of the Array to the Spinner
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, colors);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
-        spinner.setAdapter(spinnerArrayAdapter);
-
-
-        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
-        ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, colors);
-        spinnerArrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
-        spinner2.setAdapter(spinnerArrayAdapter2);
+     //  Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+     //  ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, colors);
+     //  spinnerArrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+     //  spinner2.setAdapter(spinnerArrayAdapter2);
 
 
 
