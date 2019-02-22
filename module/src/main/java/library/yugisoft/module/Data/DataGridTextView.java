1d3763c38c73897d3d1ef6acfa0554695db7795c
yugisoft <yugisoftware@gmail.com>
2019-02-22 11:31:06 +0300

    DataGridTextView

--- module/src/main/java/library/yugisoft/module/Data/DataGridTextView.java
+++ module/src/main/java/library/yugisoft/module/Data/DataGridTextView.java
@@ -184,6 +184,7 @@ public class DataGridTextView extends android.support.v7.widget.AppCompatTextVie
                 switch (getType()) {
                     case 0:
                         setText(String.format(getFormat(), caption));
+                        break;
                     case 1:
                         setText(String.format(getFormat(), Integer.parseInt(caption)));
                         if(autoAligment) setTextAlignment(TEXT_ALIGNMENT_TEXT_END);
